87e37a55fd9367762052be661013613eb94f3c71
inayarajwani <inaya.rajwani@gmail.com>
2024-01-07 20:27:21 -0500

    setting up card

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import logo from './logo.svg';
+import React from 'react';
+import {comments} from './commentData';
 import './style.css';
 
+//returning instance of Card component for every comment in commentData
+//map over comments array with the arg named comment and return instance of Card component
+//for each component, give it attribute commentObject and value {comment}
 function App() {
-  
-  
   return (
-    
+    <div>
+      {
+        //passed down a commentObject attribute to Card which contains an object with three properties (profileImg, username, and comment)
+        comments.map(comment =>
+          <Card commentObject = {comment}/>
+          )
+      }
+    </div>
   );
 }
 
--- src/Card.js
+++ src/Card.js
@@ -2,8 +2,13 @@ import React from "react";
 import Body from './Body';
 import Header from './Header';
 
-function Card(){
-
+function Card(props){
+    return (
+        <div>
+            <Header profileImg = {props.commentObject.profileImg} username={props.commentObject.username}/>
+            <Body comment={props.commentObject.comment}/>
+        </div>
+    )
 };
 
 export default Card;
